c455330b3edf1abbf13edf76caf9ea7ef10884dc
Răzvan Mîndrilă <msir3565@scs.ubbcluj.ro>
2024-09-05 18:25:11 +0300

    1

--- lab10_11_2/cmake-build-debug-coverage/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_lab6_FILE C:/Users/razva/Desktop/CV si Proiecte/OOP/lab10_11_2/cmake-build-debug-coverage/lab6.exe)
-set(__QT_DEPLOY_TARGET_lab6_RUNTIME_DLLS C:/Qt/6.7.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Core.dll)
